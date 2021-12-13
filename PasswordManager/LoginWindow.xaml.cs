56c644dcbe34be35c36c1f6c67fa8718f479c04c
Kevin <kevin.dahlhoff@students.tbs1.de>
2021-12-13 17:33:15 +0100

    initial commit

---
+++ PasswordManager/LoginWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PasswordManager
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClickBack(object sender, RoutedEventArgs e)
+        {
+            Window mainWindow = new MainWindow();
+            Close();
+            mainWindow.Show();
+        }
+
+        private void BtnClickLogin(object sender, RoutedEventArgs e)
+        {
+            Window mainWindow = new MainWindow();
+            Close();
+            mainWindow.Show();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Window registerWindow = new RegisterWindow();
+            Close();
+            registerWindow.Show();
+        }
+    }
+}
---
+++ PasswordManager/RegisterWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PasswordManager
+{
+    /// <summary>
+    /// Interaktionslogik für RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Window mainWindow = new MainWindow();
+            Close();
+            mainWindow.Show();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Window mainWindow = new MainWindow();
+            Close();
+            mainWindow.Show();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Window mainWindow = new MainWindow();
+            Close();
+            mainWindow.Show();
+        }
+    }
+}
